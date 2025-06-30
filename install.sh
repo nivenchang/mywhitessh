79778754f7186e8266addb1e2ab6e3f976544115
Chang <iwillfindmyway@gmail.com>
2025-06-30 11:32:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ SELF_URL="https://raw.githubusercontent.com/nivenchang/mywhitessh/refs/heads/mai
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
+BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 
 # --- 函数定义 ---
